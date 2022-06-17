dfb3fd68ae528f9f48713239357d8ef0367fd90d
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-06-17 08:06:00 +0000

    [Fix]crop_folder一覧画面のレイアウト修正

--- app/models/crop_folder.rb
+++ app/models/crop_folder.rb
@@ -9,4 +9,15 @@ class CropFolder < ApplicationRecord
   validates :new_crop_date, presence: true
   validates :memo, length: { maximum: 150 }
 
+  has_one_attached :crop_image
+
+  def get_crop_image(width, height)
+    unless crop_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_crop.jpeg')
+      crop_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    crop_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
+
