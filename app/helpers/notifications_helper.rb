41091a90db66f63a1542a32e5361e824aa334926
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-07-05 06:10:03 +0000

    [Fix]レスポンシブ（top、今月のおすすめ、ユーザーabout画面）

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -11,14 +11,14 @@ module NotificationsHelper
     case notification.action
       when "dm" then
        @chat = Chat.find_by(id: notification.chat_id)&.message
-        tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"があなたに"+tag.a('メッセージ',href:chat_path(notification.visitor), style:"font-weight: bold;")+"を送りました"
+        tag.a(@visitor.name, href:user_about_path(@visitor), style:"font-weight: bold;")+"があなたに"+tag.a('メッセージ',href:chat_path(notification.visitor), style:"font-weight: bold;")+"を送りました"
       when "comment" then
         @comment = CropComment.find_by(id: @visitor_comment)&.comment
-        tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの栽培作物', href:crop_folder_path(notification.crop_folder_id), style:"font-weight: bold;")+"に「コメント」しました"
+        tag.a(@visitor.name, href:user_about_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの栽培作物', href:crop_folder_path(notification.crop_folder_id), style:"font-weight: bold;")+"に「コメント」しました"
       when "favorite" then
-        tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの栽培作物', href:crop_folder_path(notification.crop_folder_id), style:"font-weight: bold;")+"に「いいね」しました"
+        tag.a(@visitor.name, href:user_about_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの栽培作物', href:crop_folder_path(notification.crop_folder_id), style:"font-weight: bold;")+"に「いいね」しました"
       when "like" then
-        tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの商品', href:item_path(notification.item_id), style:"font-weight: bold;")+"に「気になる」しました"
+        tag.a(@visitor.name, href:user_about_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの商品', href:item_path(notification.item_id), style:"font-weight: bold;")+"に「気になる」しました"
     end
   end
 end
