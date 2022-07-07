230c3b650e9532e7e752e4176a64fc89366884b7
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-07-07 08:59:21 +0000

    [Fix]商品登録・削除後の画面遷移を修正、ロゴにリンクを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.user_id = current_user.id
     if @item.save
-      redirect_to items_path, success: "栽培作物を投稿しました"
+      redirect_to user_items_path(@item.user), success: "栽培作物を投稿しました"
     else
       @items = Item.all.order(created_at: :desc).page(params[:page]).per(5)
       render "new"
@@ -33,7 +33,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to items_path(@item), success: "商品の登録内容を更新しました"
+      redirect_to user_items_path(@item.user), success: "商品の登録内容を更新しました"
     else
       render "edit"
     end
